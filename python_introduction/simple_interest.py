41c51c6ad07a110b36f3c9c1c42c6198ea586b16
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-09-14 12:19:34 -0700

    1. Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# calculate the simple interest on a given principal amount, rate of interest, and time
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+print("The simple interest is:", interest)

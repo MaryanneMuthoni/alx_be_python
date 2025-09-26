4fc4ab791481f09eee5456eeec1722109db9c9d1
Maryanne Muthoni <muthoni.w.maryanne@gmail.com>
2025-09-26 06:41:32 -0700

    0. Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+# Define a function that performs basic arithmetic operations.
+# This function, perform_operation, will be imported and used in a separate main.py script
+#+ which will be provided
+
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                print("Can not divide by 0")
+            else:
+                return num1 / num2
